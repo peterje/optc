6e019306f5250f97e878de205d7d3b663a6766e0
Peter Edmonds <petejedmonds@gmail.com>
2023-08-19 14:57:02 -0700

    added logic to add missing legends on import

--- src/data/client.ts
+++ src/data/client.ts
@@ -6,3 +6,10 @@ const defaultSettings: Settings = { hideBaseForms: false }
 const defaultLegends: Legend[] = orderedIDs.map(createLegend)
 export const [settings, setSettings] = makePersisted<Settings>(createSignal(defaultSettings), { storage: localStorage });
 export const [legends, setLegends] = makePersisted<Legend[]>(createSignal(defaultLegends), { storage: localStorage });
+
+export const addMisingLegends = () => {
+  const currentIDs = new Set(legends().map(legend => legend.id))
+  const missingIDs = orderedIDs.filter(id => !currentIDs.has(id))
+  const missingLegends = missingIDs.map(createLegend)
+  setLegends([...legends(), ...missingLegends])
+}
